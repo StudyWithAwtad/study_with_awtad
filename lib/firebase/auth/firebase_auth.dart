10b250101721c534e6731ed8e9bb6337ab4d6048
kabhamo <mkabha54@gmail.com>
2024-05-18 18:27:40 +0300

    add-firebase auth configurations and logics

---
+++ lib/firebase/auth/firebase_auth.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+// This file is dedicated to implement all the firebase authentication functions and logics
+
+// Create and login new user by providing email and password
+Future<UserCredential?> createUser(String email, String password) async {
+  try {
+    final credential =
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return credential;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+    return null;
+  } catch (e) {
+    print(e);
+    return null;
+  }
+}
