3d65796d2c54c8d3461c194a543d392929f64d60
kabha <mkabha54@gmail.com>
2024-06-06 03:07:37 +0300

    add: healthy request check

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:http/http.dart' as http;
 import 'firebase_options.dart';
-
+import 'dart:io';
 import 'package:flutter/material.dart';
 import './firebase/auth/firebase_auth.dart';
 
 late final FirebaseApp app;
 late final FirebaseAuth auth;
 late UserCredential? credentials;
+
+// This class was added to make http requests possible only in development moode
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,6 +29,8 @@ Future<void> main() async {
   } catch (e) {
     print('Failed to initialize Firebase: $e');
   }
+  // This line was added to make http requests possible only in development moode
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -67,6 +80,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // do somthing with credentials after successfully creating new user
   }
 
+  void _healthCheck() async {
+    var response = await http
+        .get(Uri.parse('https://10.0.2.2:7134/api/Users/healthcheck'));
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print('A network error occurred');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -96,6 +119,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 textStyle: const TextStyle(fontSize: 18),
               ),
               child: const Text('Custom Elevated Button'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                //_createUserHandler();
+                _healthCheck();
+              },
+              style: ElevatedButton.styleFrom(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                textStyle: const TextStyle(fontSize: 18),
+              ),
+              child: const Text('Healthy Check'),
             ),
           ],
         ),
